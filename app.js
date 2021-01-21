66d6b0dc83e739cd03425d1e5c0aecf993feea5b
gabrielopeyemi <famosipe2010@gmail.com>
2021-01-21 13:51:12 +0100

    gabriel-state

---
+++ app.js
@@ -0,0 +1,42 @@
+// const nav = document.getElementById("nav");
+// const butto = document.getElementById("btnn");
+// const closebtn = document.getElementsByClassName("closebtn") [0];
+// butto.addEventListener('click', openModal);
+const state = {
+    status: true
+};
+
+
+console.log(state.status);
+// getElementByClassname('icons-show').addEventListener('click', Jummy);
+get
+function Jummy(){
+    let NewStatus = !state.status;
+    state.status = NewStatus;
+    console.log({status: state.status})
+    
+};
+
+if (Jummy.NewStatus){
+    btnn.style.display= 'none'
+}else{
+    btn.style.display= 'none'
+}
+
+
+
+// function openModal(){
+//     nav.style.display = 'block';
+//     // btnn.style.display= 'none'
+
+// }
+
+// // console.log(openModal.status);
+// 
+// btnn.addEventListener('click', function(){
+//     icon.innerHTML = `<i style="font-size: 24px;" class="fas fa-times"></i>`
+//     console.log('working')
+//     var window.status = false;
+//     console.log(window.status);
+    
+// });
