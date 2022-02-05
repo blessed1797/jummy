1d663329ee397af3c6a2f8bd76d6b1550bc3c703
my-name <blessedlyjummy@gmail.com>
2022-02-05 18:11:31 +0100

    adjusted navbar

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 window.addEventListener('load',function(){
     initSlider(
       'slider', //id
-      ['Am a Frontend Developer','Am a data analyst','I love coding'], 
+      ["I'm a Frontend Developer","I'm a data analyst","I love coding"], 
       100, //duration
       1000 //delay
     );
